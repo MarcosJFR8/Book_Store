45c37400e302363fd28b59e568e3e955e86363ee
Marcos <marcosjfr3@gmail.com>
2024-07-20 17:59:25 -0400

    booklist on admin

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     #local
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
+    'books.apps.BooksConfig',
     # Third Party
     'crispy_forms',
     'crispy_bootstrap5',
